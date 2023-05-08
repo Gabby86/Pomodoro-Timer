4a77fe46fc9be3c3034fd25735fb554cab58a822
Gabby86 <gabrieljoe00@gmail.com>
2023-05-08 20:36:55 +0200

    first commit

--- main.js
+++ main.js
@@ -1,84 +1,109 @@
-var start = document.getElementById("start")
-var stop = document.getElementById("stop")
-var reset = document.getElementById("reset")
+// Get elements from HTML
+const wMinutes = document.getElementById('w-minutes');
+const wSeconds = document.getElementById('w-seconds');
+const bMinutes = document.getElementById('b-minutes');
+const bSeconds = document.getElementById('b-seconds');
+const counter = document.getElementById('counter');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
 
-var wm = document.getElementById("w-minutes")
-var ws = document.getElementById("w-seconds")
+let workTime = 25; // work time in minutes
+let breakTime = 5; // break time in minutes
+let cycles = 0; // number of work/break cycles completed
+let isWorkTime = true; // true if currently in work time, false if in break time
+let intervalID; // variable to hold the setInterval ID
 
-var bm = document.getElementById("b-minutes")
-var bs = document.getElementById("b-seconds")
+// Set the initial timer values
+wMinutes.innerText = workTime;
+wSeconds.innerText = '00';
+bMinutes.innerText = breakTime;
+bSeconds.innerText = '00';
 
-var counter = document.getElementById("counter")
+// Event listeners for buttons
+startBtn.addEventListener('click', startTimer);
+stopBtn.addEventListener('click', stopTimer);
+resetBtn.addEventListener('click', resetTimer);
 
-
-//store a reference to a timer variable
-
-var startTimer;
-
-start.addEventListener("click", function(){
-    if(startTimer === undefined){
-        startTimer = setInterval(timer, 1000)
-    } else{
-        alert("Timer is already running");
-    }
-})
-
-reset.addEventListener("click", function(){
-    wm.innerText = 25;
-    ws.innerText = "00";
-
-    bm.innerText = 5;
-    bs.innerText = "00";
-
-    document.getElementById("counter").innerText ="0"
-    stopInterval()
-    startTimer = undefined;
-})
-
-stop.addEventListener("click", function(){
-    stopInterval()
-    startTimer = undefined;
-})
-
-//Start timer function
-
-function timer(){
-    //Work timer countdown
-    if(ws.innerText !=0){
-        ws.innerText--;
-
-    } else if(wm.innerText != 0 && ws.innerText == 0){
-        ws.innerText = 59;
-        wm.innerText--;
-    }
-
-    //Break timer countdown
-
-    if(wm.innerText = 0 && ws.innerText == 0){
-        if(bs.innerText !=0){
-            bs.innerText--;
-
-        } else if(bm.innerText !=0 && bs.innerText == 0){
-            bs.innerText = 59;
-            bm.innerText--;
-        }
-    }
-
-    //Increment Counter by one if one full cycle is completed
-
-    if(wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0){
-        wm.innerText = 25;
-        ws.innerText = "0";
-
-        bm.innerText = 5;
-        bs.innerText = "00";
-
-        document.getElementById("counter").innerText++;
-    }
+// Functions to handle timer logic
+function startTimer() {
+  intervalID = setInterval(updateTimer, 1000);
+  startBtn.disabled = true;
 }
 
-//Stop timer function
-function stopInterval(){
-    clearInterval(startTimer);
+function stopTimer() {
+  clearInterval(intervalID);
+  startBtn.disabled = false;
 }
 
+function resetTimer() {
+  clearInterval(intervalID);
+  wMinutes.innerText = workTime;
+  wSeconds.innerText = '00';
+  bMinutes.innerText = breakTime;
+  bSeconds.innerText = '00';
+  counter.innerText = '0';
+  startBtn.disabled = false;
+  cycles = 0;
+  isWorkTime = true;
+}
+
+function updateTimer() {
+  // Update the appropriate timer
+  if (isWorkTime) {
+    updateWorkTimer();
+  } else {
+    updateBreakTimer();
+  }
+}
+
+function updateWorkTimer() {
+  let minutes = parseInt(wMinutes.innerText);
+  let seconds = parseInt(wSeconds.innerText);
+
+  if (seconds === 0) {
+    if (minutes === 0) {
+      // Work timer is done
+      cycles++;
+      counter.innerText = cycles;
+      isWorkTime = false;
+      bMinutes.innerText = breakTime;
+      bSeconds.innerText = '00';
+    } else {
+      minutes--;
+      seconds = 59;
+    }
+  } else {
+    seconds--;
+  }
+
+  wMinutes.innerText = formatTime(minutes);
+  wSeconds.innerText = formatTime(seconds);
+}
+
+function updateBreakTimer() {
+  let minutes = parseInt(bMinutes.innerText);
+  let seconds = parseInt(bSeconds.innerText);
+
+  if (seconds === 0) {
+    if (minutes === 0) {
+      // Break timer is done
+      isWorkTime = true;
+      wMinutes.innerText = workTime;
+      wSeconds.innerText = '00';
+    } else {
+      minutes--;
+      seconds = 59;
+    }
+  } else {
+    seconds--;
+  }
+
+  bMinutes.innerText = formatTime(minutes);
+  bSeconds.innerText = formatTime(seconds);
+}
+
+function formatTime(time) {
+  // Add leading zero if time is less than 10
+  return time < 10 ? `0${time}` : time;
+}
